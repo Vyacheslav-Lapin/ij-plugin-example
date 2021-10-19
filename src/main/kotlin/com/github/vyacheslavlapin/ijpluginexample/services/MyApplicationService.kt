5c4f0f69035c6b2240ec4234efea606336b7e079
GitHub Action <action@github.com>
2021-10-19 11:04:36 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/vyacheslavlapin/ijpluginexample/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.vyacheslavlapin.ijpluginexample.services
+
+import com.github.vyacheslavlapin.ijpluginexample.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/vyacheslavlapin/ijpluginexample/services/MyProjectService.kt
+++ src/main/kotlin/com/github/vyacheslavlapin/ijpluginexample/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.vyacheslavlapin.ijpluginexample.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.vyacheslavlapin.ijpluginexample.MyBundle
 
 class MyProjectService(project: Project) {
 
